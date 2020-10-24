fe57b9ad5efcd8151704ea77b0f3521530f2c6c6
juanes <juanesgym2018@gmail.com>
2020-10-23 19:03:55 -0500

    proyecto expresiones

---
+++ expresiones_regulares-master/javaindex.js
@@ -0,0 +1,10 @@
+function capturar() {
+  ca = document.getElementById("ex").value;
+  regEx = /^[A-Z]{1}[0-9]{3}[a-z]{3}\W{3}$/;
+  console.log(regEx.test(ca));
+  if (regEx.test(ca) == true) {
+    $('#dataCorrect').modal('show');
+  } else {
+    $('#incorrectData').modal('show');
+  }
+}
